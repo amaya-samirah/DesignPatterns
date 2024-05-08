f47702ae29dfdd5c1eb6c8305ae022464417eb2f
amaya-samirah <amaya.s.26292@gmail.com>
2024-05-08 01:49:28 -0400

    setSortBehavior mehthod added

--- Strategy/AppartmentList.java
+++ Strategy/AppartmentList.java
@@ -6,21 +6,32 @@ import java.util.ArrayList;
 public class AppartmentList extends Appartment implements SortBehavior{
     
     //ASK QUESTION: i have no idea what's wrong with this and what the uml means
+    // what is sortBehavior supposed to be
     //---------
-    private final ArrayList<Appartment> appartments = new ArrayList<Appartment>();
+    private ArrayList<Appartment> appartments = new ArrayList<Appartment>();
 
-    private final SortBehavior sortBehavior() = new SortBehavior();
+    private SortBehavior sortBehavior = new SortBehavior();
    //--------
 
    public AppartmentList(String listAddress, int listNumBedrooms, int listNumBathrooms, double listPrice)
    {
-        super(listAddress, listNumBedrooms, listNumBathrooms, listPrice);
+     super(listAddress, listNumBedrooms, listNumBathrooms, listPrice);
    }
    
    public void add(String address, int numBedrooms, int numBathrooms, double price)
    {
-        Appartment newAppartment = new Appartment(address, numBedrooms, numBathrooms, price);
-        appartments.add(newAppartment);
+     Appartment newAppartment = new Appartment(address, numBedrooms, numBathrooms, price);
+     appartments.add(newAppartment);
 
    }
+
+   public void setSortBehavior(SortBehavior sortBehavior)
+   {
+    //ASK QUESTION: what am i supposed to set behavior to?
+    
+
+   }
+   
+
+   
 }
