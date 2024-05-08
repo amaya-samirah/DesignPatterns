971c355f462f704f150af4ec8a926194c9fa5f42
amaya-samirah <amaya.s.26292@gmail.com>
2024-05-08 14:01:29 -0400

    Updated QuickSort

--- Strategy/QuickSort.java
+++ Strategy/QuickSort.java
@@ -14,42 +14,79 @@ public class QuickSort implements SortBehavior{
 
         return appartments;
     }
-    public void  quickSort(ArrayList<Appartment> appartments, int start, int end)
+    //-------------------quick sort process from https://stackoverflow.com/questions/33970992/quicksort-an-arraylist-of-strings -----------
+    private ArrayList<Appartment> quickSort(ArrayList<Appartment> appartments)
     {
-        if(start>=end)
+        if(appartments.isEmpty())
         {
-            return;
+            return appartments;
         }
-        int pivotIndex = partition(appartments);
-        quickSort(appartments, start, pivotIndex-1);
-        quickSort(appartments, pivotIndex+1, end);
+        ArrayList<Appartment> sortedAppartments; //the list after it's been sorted will go here
+        ArrayList<Appartment> lessThanPivot = new ArrayList<Appartment>();
+        ArrayList<Appartment> greaterThanPivot = new ArrayList<Appartment>();
 
-
-    }
-
-    public int partition(ArrayList<Appartment> appartments)
-    {
-        int appartmentsLength = appartments.size()-1;
-        Appartment pivot = appartments.get(appartmentsLength);
-        int i=0;
-        for(int j=0;i<appartmentsLength;j++)
+        Appartment pivot = appartments.get(0); //pivot will start and beginning
+        int i;
+        Appartment j;
+        for(i =1;i<appartments.size();i++)
         {
-            if(appartments.get(j).compareTo(pivot) > 0) //this appartment is less than parameter appartment
-            { 
-                //swapp
-                Appartment temp = appartments.get(i);
-                appartments.set(i, appartments.get(j));
-                appartments.set(j, temp);
-                i++;
+            j = appartments.get(i);
+            if(j.compareTo(pivot)<0)
+            {
+                lessThanPivot.add(j);
+            }
+            else
+            {
+                greaterThanPivot.add(j);
             }
         }
-        //final swap
-        Appartment temp = appartments.get(i);
-        appartments.set(i, appartments.get(appartmentsLength)); //pivot
-        appartments.set(appartmentsLength, temp);
-        return i; //returns pivot index
+        lessThanPivot = quickSort(lessThanPivot);
+        greaterThanPivot = quickSort(greaterThanPivot);
+        lessThanPivot.add(pivot);
+        lessThanPivot.addAll(greaterThanPivot);
+        sortedAppartments = lessThanPivot;
 
-
+        return sortedAppartments;
 
     }
+
+    // private void quickSort(ArrayList<Appartment> appartments, int start, int end)
+    // {
+    //     if(start>=end)
+    //     {
+    //         return;
+    //     }
+    //     int pivotIndex = partition(appartments);
+    //     quickSort(appartments, start, pivotIndex-1);
+    //     quickSort(appartments, pivotIndex+1, end);
+
+
+    // }
+
+    
+    // private int partition(ArrayList<Appartment> appartments)
+    // {
+    //     int appartmentsLength = appartments.size()-1;
+    //     Appartment pivot = appartments.get(appartmentsLength);
+    //     int i=0;
+    //     for(int j=0;j<appartmentsLength;j++)
+    //     {
+    //         if(appartments.get(j).compareTo(pivot) > 0) //this appartment is less than parameter appartment
+    //         { 
+    //             //swapp
+    //             Appartment temp = appartments.get(i);
+    //             appartments.set(i, appartments.get(j));
+    //             appartments.set(j, temp);
+    //             i++;
+    //         }
+    //     }
+    //     //final swap
+    //     Appartment temp = appartments.get(i);
+    //     appartments.set(i, appartments.get(appartmentsLength)); //pivot
+    //     appartments.set(appartmentsLength, temp);
+    //     return i; //returns pivot index
+
+
+
+    // }
 }
