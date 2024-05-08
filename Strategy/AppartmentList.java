3e3671cc35e3c855c7f1392bc7de1d6fa390fc64
amaya-samirah <amaya.s.26292@gmail.com>
2024-05-08 01:36:19 -0400

    AppartmentList constructor created

--- Strategy/AppartmentList.java
+++ Strategy/AppartmentList.java
@@ -2,11 +2,20 @@
 //
 import java.util.ArrayList;
 
- private final ArrayList<Appartment> appartments = new ArrayList<Appartment>();
+
 public class AppartmentList extends Appartment implements SortBehavior{
     
-   
+    //ASK QUESTION: i have no idea what's wrong with this and what the uml means
+    //---------
+    private final ArrayList<Appartment> appartments = new ArrayList<Appartment>();
 
     private final SortBehavior sortBehavior() = new SortBehavior();
+   //--------
+
+   public AppartmentList(String listAddress, int listNumBedrooms, int listNumBathrooms, double listPrice)
+   {
+        super(listAddress, listNumBedrooms, listNumBathrooms, listPrice);
+   }
+   
    
 }
