0eed3e4c25e9f47d9a8bd45d0e300e13b931c00d
amaya-samirah <amaya.s.26292@gmail.com>
2024-05-08 01:25:28 -0400

    AppartmentList created

---
+++ Strategy/AppartmentList.java
@@ -0,0 +1,12 @@
+//Amaya Shabazz 2024 CSCE 247
+//
+import java.util.ArrayList;
+
+ private final ArrayList<Appartment> appartments = new ArrayList<Appartment>();
+public class AppartmentList extends Appartment implements SortBehavior{
+    
+   
+
+    private final SortBehavior sortBehavior() = new SortBehavior();
+   
+}
