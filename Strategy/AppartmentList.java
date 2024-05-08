90f25d252fe7f76e53b4394118773333ece8e3dc
amaya-samirah <amaya.s.26292@gmail.com>
2024-05-08 14:21:21 -0400

    Fixed formatting

--- Strategy/AppartmentList.java
+++ Strategy/AppartmentList.java
@@ -4,43 +4,29 @@ import java.util.ArrayList;
 
 
 public class AppartmentList{
-    
-    //ASK QUESTION: i have no idea what's wrong with this and what the uml means
-    // what is sortBehavior supposed to be
-    //---------
     private ArrayList<Appartment> appartments;
 
     private SortBehavior sortBehavior;  
-   //--------
-
    
-   //the uml doesn't give parameters so is this right?? it's an error if i don't use the paremeters
-   //i need a defualt constructor but it wont work becuase extends Appartment which doesn't have a default constructor
    public AppartmentList()
    {
         appartments = new ArrayList<Appartment>();
         sortBehavior = new BubbleSort();
    }
 
-   
    public void add(String address, int numBedrooms, int numBathrooms, double price)
    {
      Appartment newAppartment = new Appartment(address, numBedrooms, numBathrooms, price);
      appartments.add(newAppartment);
-
    }
 
    public void setSortBehavior(SortBehavior sortBehavior)
    {
-    //ASK QUESTION: what am i supposed to set behavior to?
     this.sortBehavior = sortBehavior;
-
    }
 
    public ArrayList<Appartment> getSortedList()
    {
-    // sortBehavior.sort(appartments); //ASK QUESTION: am i using the right variable?
-    // return appartments; 
     return sortBehavior.sort(appartments);
    }
 
--- Strategy/QuickSort.java
+++ Strategy/QuickSort.java
@@ -4,12 +4,11 @@ import java.util.ArrayList;
 
 public class QuickSort implements SortBehavior{
     
-    //ASK QUESTION: is this the correct way to implement quicksort? Unsure what to do when it comes to arrayList
+    
    @Override
     public ArrayList<Appartment> sort(ArrayList<Appartment> appartments)
     {
-    //    int start = 0;
-    //    int end = appartments.size()-1;
+
         appartments = quickSort(appartments);
 
         return appartments;
@@ -27,17 +26,17 @@ public class QuickSort implements SortBehavior{
 
         Appartment pivot = appartments.get(0); //pivot will start and beginning
         int i;
-        Appartment j;
+        Appartment tempAppartment;
         for(i =1;i<appartments.size();i++)
         {
-            j = appartments.get(i);
-            if(j.compareTo(pivot)>0)
+            tempAppartment = appartments.get(i);
+            if(tempAppartment.compareTo(pivot)>0)
             {
-                lessThanPivot.add(j);
+                lessThanPivot.add(tempAppartment);
             }
-            else if(j.compareTo(pivot)<0)
+            else if(tempAppartment.compareTo(pivot)<0)
             {
-                greaterThanPivot.add(j);
+                greaterThanPivot.add(tempAppartment);
             }
         }
         lessThanPivot = quickSort(lessThanPivot);
@@ -49,44 +48,4 @@ public class QuickSort implements SortBehavior{
         return sortedAppartments;
 
     }
-
-    // private void quickSort(ArrayList<Appartment> appartments, int start, int end)
-    // {
-    //     if(start>=end)
-    //     {
-    //         return;
-    //     }
-    //     int pivotIndex = partition(appartments);
-    //     quickSort(appartments, start, pivotIndex-1);
-    //     quickSort(appartments, pivotIndex+1, end);
-
-
-    // }
-
-    
-    // private int partition(ArrayList<Appartment> appartments)
-    // {
-    //     int appartmentsLength = appartments.size()-1;
-    //     Appartment pivot = appartments.get(appartmentsLength);
-    //     int i=0;
-    //     for(int j=0;j<appartmentsLength;j++)
-    //     {
-    //         if(appartments.get(j).compareTo(pivot) > 0) //this appartment is less than parameter appartment
-    //         { 
-    //             //swapp
-    //             Appartment temp = appartments.get(i);
-    //             appartments.set(i, appartments.get(j));
-    //             appartments.set(j, temp);
-    //             i++;
-    //         }
-    //     }
-    //     //final swap
-    //     Appartment temp = appartments.get(i);
-    //     appartments.set(i, appartments.get(appartmentsLength)); //pivot
-    //     appartments.set(appartmentsLength, temp);
-    //     return i; //returns pivot index
-
-
-
-    // }
 }
