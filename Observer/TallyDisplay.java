66622b7b4e09f476552ad4d38d47190f49464c6b
amaya-samirah <amaya.s.26292@gmail.com>
2024-05-09 18:58:38 -0400

    TallyDisplay created & methods added

---
+++ Observer/TallyDisplay.java
@@ -0,0 +1,28 @@
+/**
+ * Holds the information for the tally display
+ * @author Amaya Shabazz
+ */
+import java.util.ArrayList;
+public class TallyDisplay implements Observer{
+    private Subject poll;
+    private ArrayList<Candidate> candidates;
+
+    /**
+     * Allows to create an instance of TallyDisplay
+     * @param poll Holds the Subject
+     */
+    public TallyDisplay(Subject poll)
+    {
+        
+
+    }
+    public void update(ArrayList<Candidate> candidates)
+    {
+
+    }
+
+    private void display()
+    {
+        
+    }
+}
