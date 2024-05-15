838efee0476735c740d1e81abb91b94465eaf0ea
amaya-samirah <amaya.s.26292@gmail.com>
2024-05-15 18:25:00 -0400

    Completed SpecialChars

--- Decorator/SpecialChars.java
+++ Decorator/SpecialChars.java
@@ -1,8 +1,9 @@
+import java.util.Random;
+
 /**
  * Holds the information to decorate password ith special characters
  * @author Amaya Shabazz
  */
-import java.util.Random;
 public class SpecialChars extends PasswordDecorator{
     
     /**
@@ -12,9 +13,57 @@ public class SpecialChars extends PasswordDecorator{
     public SpecialChars(Password passwordBeginning)
     {
         super(passwordBeginning);
-
+        Random random = new Random();
+        String[] temp = getPassword().split("");
+        for(int index = 0;index<temp.length;index++)
+        {
+            int chance = random.nextInt(0, 10); //0,1,2 will be the chance for special characters (3/10 = 30%)
+            if(chance==0 || chance==1 || chance==2) //special character added
+            {
+                int type = random.nextInt(0, 7); //each number represents a special character
+                switch (type) {
+                    case 0:
+                        temp[index] = temp[index].concat("*");
+                        break;
+                    case 1:
+                        temp[index] = temp[index].concat("!");
+                        break;
+                    case 2:
+                        temp[index] = temp[index].concat("%");
+                        break;
+                    case 3:
+                        temp[index] = temp[index].concat("+");
+                        break;
+                    case 4:
+                        temp[index] = temp[index].concat(".");
+                        break;
+                    case 5:
+                        temp[index] = temp[index].concat("{");
+                        break;
+                    case 6:
+                        temp[index] = temp[index].concat("}");
+                        break;
+                    default:
+                        break;
+                }
+            }
+            else
+            {
+                return;
+            }
+        }
+        for(int index = 0;index<temp.length;index++)
+        {
+            if(index!=temp.length-1)
+            {
+               password = temp[index].concat(temp[index+1]);
+            }
+            else
+            {
+                password = temp[index-1].concat(temp[index]);
+            }
+        }
     }
-   
 
     /**
      * Accesses the password
