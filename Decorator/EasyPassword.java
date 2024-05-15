09030103f0b082fb5f0a6994542c2bf7aa25b0b6
amaya-samirah <amaya.s.26292@gmail.com>
2024-05-15 17:19:28 -0400

    EasyPassword completed

--- Decorator/EasyPassword.java
+++ Decorator/EasyPassword.java
@@ -2,21 +2,30 @@
  * Holds the information for an easy password
  * @author Amaya Shabazz
  */
+import java.util.Random;
 public class EasyPassword extends Password{
 
     /**
      * Constucts an easy password
-     * @param phrase Will return the new password
+     * @param phrase Holds the original password
      */
     public EasyPassword(String phrase)
     {
+        Random random = new Random();
+        int number = random.nextInt(0, 101);
         String[] phraseArray = phrase.split(" ");
       
         for(int size =0;size<phraseArray.length;size++)
-        {
-            System.out.println(phraseArray[size]+"-");
+        {   
+            if(size==phraseArray.length-1)
+            {
+                System.out.println(phraseArray[size]+"-"+number);
+            }
+            else
+            {
+                System.out.println(phraseArray[size]+"-");
+            }
         }
-
     }
 
     /**
