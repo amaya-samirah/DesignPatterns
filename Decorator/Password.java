6ff61f1b0c4bd3f64f1cb714ffd3a43264e95b42
amaya-samirah <amaya.s.26292@gmail.com>
2024-05-15 12:29:27 -0400

    Created Password

---
+++ Decorator/Password.java
@@ -0,0 +1,13 @@
+/**
+ * Holds the basic password information
+ * @author Amaya Shabazz
+ */
+public abstract class Password {
+    protected String password;
+    
+    public String getPassword()
+    {
+        return this.password;
+    }
+    
+}
