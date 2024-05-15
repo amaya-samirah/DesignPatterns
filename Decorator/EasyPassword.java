2074277cbaf56980b8b7fdfc32f5b414125319e7
amaya-samirah <amaya.s.26292@gmail.com>
2024-05-15 12:47:20 -0400

    Created EasyPassword

---
+++ Decorator/EasyPassword.java
@@ -0,0 +1,17 @@
+/**
+ * Holds the information for an easy password
+ * @author Amaya Shabazz
+ */
+public class EasyPassword extends Password{
+
+    public EasyPassword(String phrase)
+    {
+        password = phrase;
+
+    }
+
+    public String getPassword()
+    {
+        return password;
+    }
+}
