56abe87cb4b0353c4e1196ca655e50eeb2cde273
amaya-samirah <amaya.s.26292@gmail.com>
2024-05-18 17:25:05 -0400

    Updated State:

--- State/State.java
+++ State/State.java
@@ -4,5 +4,25 @@
  */
 import java.util.ArrayList;
 public abstract class State {
-    
+    protected MusicBox box;
+    private ArrayList<String> starLyrics;
+    private ArrayList<String> happyLyrics;
+
+    /**
+     * This creates a new instance of state
+     * @param box Holds the music box infromation
+     * @param twinkleFileName Holds the file name for twinkle twinkle little star
+     * @param happFileName Holds the file name for happy birthday
+     */
+    public State(MusicBox box, String twinkleFileName, String happFileName)
+    {
+        this.box = box;
+        this.starLyrics = FileReader.getLyrics(twinkleFileName);
+        this.happyLyrics = FileReader.getLyrics(happFileName);
+    }
+
+    public void pressStartButton()
+    {
+        
+    }
 }
