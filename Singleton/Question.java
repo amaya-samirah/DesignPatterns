6d8dc981fc683f6b0ebd53983cb041eccd18478d
amaya-samirah <amaya.s.26292@gmail.com>
2024-05-20 20:23:50 -0400

    Question added

---
+++ Singleton/Question.java
@@ -0,0 +1,58 @@
+/**
+ * Holds the questions for the game
+ * @author Amaya Shabazz
+ */
+public class Question {
+    private static final String ANSI_GREEN = "\u001B[32m";
+    private static final String ANSI_RED = "\u001B[31m";
+    private static final String ANSI_BLACK = "\u001B[30m";
+
+    private int num1;
+    private int num2;
+    private int answer;
+    private int userAnswer;
+    private Operand operand;
+
+    /**
+     * Constructs a new instance of question
+     */
+    public Question()
+    {
+
+    }
+
+    /**
+     * Accesses the question
+     * @return Will return a question
+     */
+    public String getQuestion()
+    {
+
+    }
+
+    /**
+     * Sets the user's answer
+     * @param answer Holds the user's answer
+     */
+    public void setUserAnswer(int answer)
+    {
+
+    }
+
+    /*
+     * Returns true or false if the answer is right
+     */
+    public boolean isCorrect()
+    {
+
+    }
+
+    /**
+     * Prints the necessary info to the console
+     */
+    public String toString()
+    {
+
+    }
+    
+}
