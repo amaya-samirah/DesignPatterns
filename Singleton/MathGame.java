2a11fa8e4da8caf690d2003d7e48c446aadd017c
amaya-samirah <amaya.s.26292@gmail.com>
2024-05-20 20:31:06 -0400

    MathGame added

---
+++ Singleton/MathGame.java
@@ -0,0 +1,38 @@
+/**
+ * Holds the information for the math game
+ * @author Amaya Shabazz
+ */
+import java.util.ArrayList;
+public class MathGame {
+    private static int NUM_QUESTIONS;
+    private MathGame mathGame;
+    private ArrayList<Question> questions;
+
+    /**
+     * Constructs a new instance of math game for the class
+     */
+    private MathGame()
+    {
+        
+    }
+
+    /**
+     * Constructs a new instane of math game for outside classes
+     * @return
+     */
+    public MathGame getInstance()
+    {
+        mathGame = new MathGame();
+        return mathGame;
+
+    }
+
+    /**
+     * Accesses the iterator information
+     * @return Will return an iteration of question
+     */
+    public Iterator<Question> getIterator()
+    {
+        
+    }
+}
